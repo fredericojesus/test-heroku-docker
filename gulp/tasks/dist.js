ea9f67a12a11bb34eb9bc219a394f5a2ac04f3b0
fredericojesus <fredericojesus88@gmail.com>
2016-11-04 00:19:28 +0000

    changed gulp to work on heroku

--- gulp/tasks/dist.js
+++ gulp/tasks/dist.js
@@ -56,17 +56,3 @@ gulp.task('build-dist', function() {
         notify(msg);
     }
 });
-
-gulp.task('serve-dist', function() {
-    global.isDist = true;
-
-    runSequence('clean-dist', ['optimize', 'images'], serveBuild);
-
-    function serveBuild() {
-        $.connect.server({
-            root: 'dist',
-            port: process.env.PORT || 5000,
-            livereload: false
-        });
-    }
-});
