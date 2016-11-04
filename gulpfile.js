c41234f7f81ddea2089a3d5831a30a760c8c43f6
fredericojesus <fredericojesus88@gmail.com>
2016-11-04 00:36:46 +0000

    fixed heroku

--- gulpfile.js
+++ gulpfile.js
@@ -12,20 +12,3 @@
 global.isProd = false;
 
 require('./gulp');
-var gulp = require('gulp');
-var runSequence = require('run-sequence');
-var $ = require('gulp-load-plugins')({lazy: true});
-
-gulp.task('serve-dist', function() {
-    global.isDist = true;
-
-    runSequence('clean-dist', ['optimize', 'images'], serveBuild);
-
-    function serveBuild() {
-        $.connect.server({
-            root: 'dist',
-            port: process.env.PORT || 5000,
-            livereload: false
-        });
-    }
-});
